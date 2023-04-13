27fb8d4ebcb2fa64870fa965731a0b4747b91ef1
leandrojamir <leandrojamir@hotmail.com>
2023-04-13 15:30:45 -0300

    feat: creating the fetch function

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,33 +1,42 @@
+import requests
+import time
+
+
 # 1 - Crie a função fetch
-# local: tech_news/scraper.py
-#  Antes de fazer scrape, precisamos de uma página! Esta função será
-# responsável por fazer a requisição HTTP ao site e obter o conteúdo HTML.
-#  Alguns cuidados deverão ser tomados: como a nossa função poderá ser
-# utilizada várias vezes em sucessão, na nossa implementação devemos nos
-# assegurar que um Rate Limit será respeitado.
-#  A função deve receber uma URL
-#  A função deve fazer uma requisição HTTP get para esta URL utilizando a
-# função requests.get
-#  A função deve retornar o conteúdo HTML da resposta.
-#  A função deve respeitar um Rate Limit de 1 requisição por segundo; Ou seja,
-# caso chamada múltiplas vezes, ela deve aguardar 1 segundo entre cada
-# requisição que fizer. Dica: Uma forma simples de garantir que cada
-# requisição seja feita com um intervalo mínimo de um segundo é utilizar
-# time.sleep(1) antes de cada requisição.
-# (Existem outras formas mais eficientes.)
-#  Caso a requisição seja bem sucedida com Status Code 200: OK, deve ser
-# retornado seu conteúdo de texto;
-#  Caso a resposta tenha o código de status diferente de 200,
-# deve-se retornar None;
-#  Caso a requisição não receba resposta em até 3 segundos, ela deve ser
-# abandonada (este caso é conhecido como "Timeout") e a função
-# deve retornar None.
-#  Você vai precisar definir o header user-agent para que a raspagem do blog
-# da Trybe funcione corretamente. Para isso, preencha com o valor
-# "Fake user-agent" conforme exemplo abaixo:
-# { "user-agent": "Fake user-agent" }
-def fetch(url):
-    """Seu código deve vir aqui"""
+# A função deve receber uma URL
+def fetch(url: str):
+    #  Você vai precisar definir o header user-agent para que a raspagem do
+    # blog da Trybe funcione corretamente. Para isso, preencha com o valor
+    # "Fake user-agent" conforme exemplo abaixo:
+    # { "user-agent": "Fake user-agent" }
+    header = {"user-agent": "Fake user-agent"}
+    try:
+        #  A função deve fazer uma requisição HTTP get para esta URL
+        # utilizando a função requests.get
+        #  Caso a requisição não receba resposta em até 3 segundos, ela deve
+        # ser abandonada (este caso é conhecido como "Timeout")
+        response = requests.get(url, timeout=3, headers=header)
+        print(f"\n>res:{response}")
+        # A função deve retornar o conteúdo HTML da resposta.
+        print(f"\n>raise http error:{response.raise_for_status}")
+        print(f"\n>text:{response.text}")
+        response.raise_for_status()
+        #  A função deve respeitar um Rate Limit de 1 requisição por segundo;
+        # Ou seja, caso chamada múltiplas vezes, ela deve aguardar 1 segundo
+        # entre cada requisição que fizer. Dica: Uma forma simples de garantir
+        # que cada requisição seja feita com um intervalo mínimo de um segundo
+        # é utilizar time.sleep(1) antes de cada requisição.
+        # (Existem outras formas mais eficientes.)
+        time.sleep(1)
+    except (requests.ReadTimeout, requests.HTTPError):
+        #  Caso a resposta tenha o código de status diferente de 200,
+        # deve-se retornar None;
+        # e a função deve retornar None.
+        return None
+    print(f"\n>200 content text:{response.text}")
+    #  Caso a requisição seja bem sucedida com Status Code 200: OK, deve ser
+    # retornado seu conteúdo de texto;
+    return response.text
 
 
 # Requisito 2
