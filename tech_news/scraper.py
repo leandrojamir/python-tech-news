331ef41267e507574e2bdb2c25250969c573ad76
leandrojamir <leandrojamir@hotmail.com>
2023-04-14 17:09:14 -0300

    feat: creating the scrape_news function

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -92,7 +92,6 @@ def scrape_next_page_link(html_content):
         selector = Selector(text=html_content)
         # <span aria-current="page" class="page-numbers current">1</span>
         page = selector.css(".page-numbers.current::text").get()
-        print(f"\nestou na pagina:{page}")
     except (requests.HTTPError):
         return None
     if not page:
@@ -100,25 +99,106 @@ def scrape_next_page_link(html_content):
     else:
         next_page = int(page) + 1
     # A função deve retornar a URL obtida.
-    print(f"{URL_BASE}/page/{str(next_page)}/")
     next_page_url = f"{URL_BASE}/page/{str(next_page)}/"
     return next_page_url
 
 
-# tests/test_scraper.py::test_scrape_next_page_link
-# estou na pagina:1
-# https://blog.betrybe.com/page/2/
-
-# estou na pagina:2
-# https://blog.betrybe.com/page/3/
-
-# estou na pagina:None
-# PASSED
-
-
-# Requisito 4
+# 4 - Crie a função scrape_news
+#  Agora que sabemos pegar páginas HTML, e descobrir o link de notícias,
+# é hora de fazer o scrape dos dados que procuramos!
+# É bom saber que ao fazer scraping na vida real, você está sempre "refém" de
+# quem construiu o site. Por exemplo, pode ser que nem toda notícia tenha
+# exatamente o mesmo HTML/CSS e você precise de criatividade para contornar.
+#  A função deve receber como parâmetro o conteúdo HTML da página de uma
+# única notícia
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    #  A função deve, no conteúdo recebido, buscar as informações das notícias
+    # para preencher um dicionário com os seguintes atributos:
+    # url - link para acesso da notícia.
+    # <link rel="canonical" href="https://blog.betrybe.com/tecnologia/
+    # cabos-de-rede/">
+    url = selector.css("link[rel='canonical']::attr(href)").get()
+    print(f"\n'url': '{url}'")
+    # title - título da notícia.
+    # <h1 class="entry-title">Cabos de rede: o que são, quais os tipos e como
+    # crimpar?</h1>
+    title = selector.css(".entry-title::text").get()
+    print(f"\n'title': '{title}'")
+    # timestamp - data da notícia, no formato dd/mm/AAAA.
+    # <li class="meta-date">10/04/2023</li>
+    timestamp = selector.css(".meta-date::text").get()
+    print(f"\n'timestamp': '{timestamp}'")
+    # writer - nome da pessoa autora da notícia.
+    # <a class="url fn n" href="https://blog.betrybe.com/author/dayane-arena
+    # -dos-santos/" title="View all posts by Dayane Arena dos Santos">Dayane
+    # Arena dos Santos</a>
+    writer = selector.css(".url.fn.n::text").get()
+    print(f"\n'writer': '{writer}'")
+    # reading_time - número de minutos necessários para leitura.
+    # <li class="meta-reading-time"><i class="cs-icon cs-icon-clock"></i>
+    # 9 minutos de leitura</li>
+    reading_time = selector.css(".meta-reading-time::text").get().split()[0]
+    print(type(reading_time))
+    reading_time_int = int(reading_time)
+    print(f"\n'reading_time': '{reading_time}'")
+    # 'reading_time': '5 minutos de leitura'
+    # 'reading_time': '['15', 'minutos', 'de', 'leitura']'
+    # summary - o primeiro parágrafo da notícia.
+    # #main > article > div > div.entry-content-wrap > div.entry-content >
+    # p:nth-child(1)
+    # <div class="entry-content">
+    # <p>...</p>
+    summary = selector.css(
+        # dica do Isaac https://www.w3schools.com/cssreF/css_selectors.php
+        # https://www.w3schools.com/cssreF/sel_first-of-type.php
+        # :first-of-type	p:first-of-type	Selects every <p> element that
+        # is the first <p> element of its parent
+        #  Caso uma tag possua outras tags aninhadas, você pode usar o
+        # seletor * para obter informações da tag ancestral e também de
+        # suas tags descendentes.
+        "div.entry-content:first-of-type > p:first-of-type *::text"
+    ).getall()
+    # https://www.youtube.com/watch?v=vuLNc2yCNYk 16:52 // 35:34 //p//text()
+    # //*[@id="main"]/article/div/div[2]/div[1]/p[1]/text()[1]
+    # summary = selector.xpath("//p[1][text()]").get()
+    print(f"\n'summary': '{summary}'")
+    # só esta passando com getall porem recebo lista de paragrafos
+    # 'summary': '['Desde o surgimento do computador e seus respectivos
+    # category - categoria da notícia.
+    category = selector.css(".category-style .label::text").get()
+    # Exemplo de um retorno da função com uma notícia fictícia:
+    # {
+    #   "url": "https://blog.betrybe.com/novidades/noticia-bacana",
+    #   "title": "Notícia bacana",
+    #   "timestamp": "04/04/2021",
+    #   "writer": "Eu",
+    #   "reading_time": 4,
+    #   "summary": "Algo muito bacana aconteceu",
+    #   "category": "Ferramentas",
+    # }
+    single_news = {
+        "url": url,
+        #  Os textos coletados em title e summary podem conter alguns
+        # caracteres vazios ao final. O teste espera que esses caracteres
+        # sejam removidos.
+        "title": title.strip(),
+        "timestamp": timestamp,
+        "writer": writer,
+        #  Muita atenção aos tipos dos campos, por exemplo, category é uma
+        # string enquanto reading_time é numérico.
+        "reading_time": reading_time_int,
+        #  Os textos coletados em title e summary podem conter alguns
+        # caracteres vazios ao final. O teste espera que esses caracteres
+        # sejam removidos e também aguarda uma string unica caso receba uma
+        # lista de string/paragrafos, como usei getall e montei essa lista[0]
+        # posso usar join e concatenar numa unica string
+        "summary": "".join(summary).strip(),
+        "category": category,
+    }
+    print(f"\numa unica noticia:\n{single_news}")
+
+    return single_news
 
 
 # Requisito 5
